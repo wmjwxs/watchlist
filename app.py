ab1937f3fc89390f9a57c7a9e3b8668675595c44
xianshu Wang <wang_xianshu@foxmail.com>
2023-10-19 15:46:25 +0800

    restart

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User page'
+
+# @app.route('/test')
+# def test_url_for():
+#
